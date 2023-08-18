495c418c587ddc3d53c8567ac388d09324b26d00
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 14:10:34 -0600

    ContentLayout created

--- pages/overview/index.tsx
+++ pages/overview/index.tsx
@@ -1,17 +1,16 @@
-import NavLayout from '@/components/layouts/nav_layout';
-import DesktopHeader from '@/components/header/header_desktop';
+import { simpleCardsData } from '@/utils/data/simple_cards_data';
+import SimpleCards from '@/components/overview/simple_cards';
+import ContentLayout from '@/components/layouts/content_layout';
 
 const index = () => {
   return (
-    <NavLayout>
-      <div className="p-10">
-        <DesktopHeader
-          label="Overview"
-          userName="Jones Ferdinand"
-          userImg="https://s3-alpha-sig.figma.com/img/35b3/2335/854ac739a1a54b9219037b25b41868ec?Expires=1693180800&Signature=GyI6qIZHaEP6BgnSHk0fiaJOX~fpiyoz2rMw4QbUcsazqUMdBKITeSjzcyXTvzYeIGHpx-LeFll1cfBNPNMII9JnriBTcA-zXwJI6XVmw1M4tqT1fJxRQc0LVPse5JOhuUlJivKEQyUzHUk8T792w86JA1~p9kDWdXmwpuZ3QMxTzJd1C-UbYjVed7t7Uka6JSz8ATL6CxwQh1OfIstWWKOt07d6tFkF7z1TAGsmGq9oPu1jZYQs0H1-unO5qTwuH1U2l0NmFjEKij~A-VpTr6uBWWiQ6VQXCDnBH7ucnMPrubSFCH2t1~9PjupdafOPDSa~bUNtnCaAxfjXrpfHxA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        />
-      </div>
-    </NavLayout>
+    <ContentLayout
+      label="Overview"
+      userName="Jones Ferdinand"
+      userImg="https://s3-alpha-sig.figma.com/img/35b3/2335/854ac739a1a54b9219037b25b41868ec?Expires=1693180800&Signature=GyI6qIZHaEP6BgnSHk0fiaJOX~fpiyoz2rMw4QbUcsazqUMdBKITeSjzcyXTvzYeIGHpx-LeFll1cfBNPNMII9JnriBTcA-zXwJI6XVmw1M4tqT1fJxRQc0LVPse5JOhuUlJivKEQyUzHUk8T792w86JA1~p9kDWdXmwpuZ3QMxTzJd1C-UbYjVed7t7Uka6JSz8ATL6CxwQh1OfIstWWKOt07d6tFkF7z1TAGsmGq9oPu1jZYQs0H1-unO5qTwuH1U2l0NmFjEKij~A-VpTr6uBWWiQ6VQXCDnBH7ucnMPrubSFCH2t1~9PjupdafOPDSa~bUNtnCaAxfjXrpfHxA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+    >
+      <SimpleCards data={simpleCardsData} />
+    </ContentLayout>
   );
 };
 

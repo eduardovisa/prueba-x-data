016e86a97d8eda2cf6d3d382dbc7c07004cf3180
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 11:07:04 -0600

    NavIcon component created

--- src/svg/divider.tsx
+++ src/svg/divider.tsx
@@ -7,7 +7,7 @@ export const DividerIcon = () => {
       height="2"
       viewBox="0 0 255 2"
       fill="none"
-      className="mt-6 hidden sm:block"
+      className="mt-6 hidden sm:block mb-6"
     >
       <g opacity="0.06">
         <path d="M0 1H255" stroke="#DFE0EB" />

b88e1d79ee36b5aac789ed117f29904cb538045d
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 17:44:58 -0600

    Tickets compopnent created

---
+++ src/utils/data/overview/tickets.ts
@@ -0,0 +1,8 @@
+import { CardsProps } from '@/types/data/overview';
+
+export const ticketsData: CardsProps[] = [
+  { label: 'Waiting on Feature Request', secondaryLabel: '4238' },
+  { label: 'Awaiting Customer Response', secondaryLabel: '1005' },
+  { label: 'Awaiting Developer Fix', secondaryLabel: '914' },
+  { label: 'Pending', secondaryLabel: '281' },
+];

eb9a796509a24c7b35b0fa02d7866b52d82e18d6
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 11:01:35 -0600

    Dynamic styles in nav links

--- src/components/nav/nav_list.tsx
+++ src/components/nav/nav_list.tsx
@@ -1,7 +1,8 @@
-import { DividerIcon } from '@/svg/divider';
-import RoutesProps from '@/types/routes_types';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { DividerIcon } from '@/svg/divider';
+import { NavIcon } from '@/svg/routes_icons';
+import Link from 'next/link';
+import RoutesProps from '@/types/routes_types';
 
 const navList = (routes: RoutesProps[], isExtra?: boolean) => {
   const router = useRouter();
@@ -12,15 +13,31 @@ const navList = (routes: RoutesProps[], isExtra?: boolean) => {
   routes.map((route, index) => {
     nav_list.push(
       <Link
-        key={`${isExtra ? 'extra' : 'main'}-link-route-${index}`}
+        key={`${isExtra ? 'extra' : 'main'}-link-route-${index} w-screen`}
         href={route.path}
       >
-        <li className={`flex mb-2 mt-10 items-center justify-center`}>
+        <li
+          className={`flex items-center justify-center ${
+            router.pathname == route.path && 'border-l-4 border-primary-white'
+          } lg:w-[255px] sm:h-[56px]`}
+        >
           <div className="flex-none w-20 hidden sm:block">
-            <div className="flex items-center justify-center">{route.icon}</div>
+            <div className="flex items-center justify-center text-white">
+              <NavIcon
+                children={route.icon}
+                color={`${router.pathname == route.path ? 'white' : '#9FA2B4'}`}
+                opacity={`${router.pathname == route.path ? '1' : '0.4'}`}
+              />
+            </div>
           </div>
           <div className="hidden lg:block flex-1">
-            <h1 className="text-[16px] font-[400] text-secondary-gray/70 tracking-[0.2px] leading-normal">
+            <h1
+              className={`text-[16px] font-[400] leading-normal ${
+                router.pathname == route.path
+                  ? 'text-primary-white'
+                  : 'text-secondary-gray/70 tracking-[0.2px]'
+              }`}
+            >
               {route.label}
             </h1>
           </div>
--- src/utils/data/routes.tsx
+++ src/utils/data/routes.tsx
@@ -14,7 +14,7 @@ const routes: RoutesProps[] = [
   {
     label: 'Overview',
     icon: <OverviewIcon />,
-    path: '#',
+    path: '/overview',
   },
   {
     label: 'Tickets',

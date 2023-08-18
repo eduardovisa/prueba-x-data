0d6ea1ee427145d43201541625719d88d4fd389b
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 00:31:37 -0600

    Logo svg changes

--- src/components/login_form.tsx
+++ src/components/login_form.tsx
@@ -1,14 +1,13 @@
-import { Logo } from '@/svg/logo';
+import { LogoLogin } from '@/svg/logo';
 import Input from './form/input';
 import LoginLayout from './layouts/login_layout';
 import { loginUser } from '@/utils/functions/login';
 
 const Login = () => {
-
   return (
     <LoginLayout>
       <a href="#" className="flex items-center mb-2">
-        <Logo />
+        <LogoLogin />
       </a>
       <h1 className="text-[19px] font-[700] text-secondary-gray/70 tracking-[0.4px] mb-6 leading-normal">
         Dashboard Kit
@@ -19,9 +18,22 @@ const Login = () => {
       <h1 className="text-[14px] font-[400] text-secondary-gray-light tracking-[0.3px] mb-6 leading-normal">
         Enter your email and password below
       </h1>
-      <form className="space-y-4 sm:space-y-6" onSubmit={(event) => loginUser(event)}>
-        <Input name="email" type="email" placeholder="Email address" label="EMAIL" />
-        <Input name="password" type="password" placeholder="Password" label="PASSWORD" />
+      <form
+        className="space-y-4 sm:space-y-6"
+        onSubmit={(event) => loginUser(event)}
+      >
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email address"
+          label="EMAIL"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          label="PASSWORD"
+        />
         <button
           type="submit"
           className="w-316 h-48 sm:w-536 lg:w-316 rounded-[8px] text-white bg-primary-blue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"

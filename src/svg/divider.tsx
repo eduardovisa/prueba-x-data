c6c14a41ccb80753e3b371e6bbfd0f57a50942e5
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 09:37:34 -0600

    Added icons to nav routes

---
+++ src/svg/divider.tsx
@@ -0,0 +1,17 @@
+export const DividerIcon = () => {
+  return (
+    <svg
+      key={`divider-nav`}
+      xmlns="http://www.w3.org/2000/svg"
+      width="255"
+      height="2"
+      viewBox="0 0 255 2"
+      fill="none"
+      className="mt-6 hidden sm:block"
+    >
+      <g opacity="0.06">
+        <path d="M0 1H255" stroke="#DFE0EB" />
+      </g>
+    </svg>
+  );
+};
---
+++ src/utils/data/routes.tsx
@@ -0,0 +1,58 @@
+import {
+  AgentsIcon,
+  ArticlesIcon,
+  ContactsIcon,
+  IdeasIcon,
+  OverviewIcon,
+  SettingsIcon,
+  SubscriptionIcon,
+  TicketsIcon,
+} from '@/svg/routes_icons';
+import RoutesProps from '@/types/routes_types';
+
+const routes: RoutesProps[] = [
+  {
+    label: 'Overview',
+    icon: <OverviewIcon />,
+    path: '#',
+  },
+  {
+    label: 'Tickets',
+    icon: <TicketsIcon />,
+    path: '#',
+  },
+  {
+    label: 'Ideas',
+    icon: <IdeasIcon />,
+    path: '#',
+  },
+  {
+    label: 'Contacts',
+    icon: <ContactsIcon />,
+    path: '#',
+  },
+  {
+    label: 'Agents',
+    icon: <AgentsIcon />,
+    path: '#',
+  },
+  {
+    label: 'Articles',
+    icon: <ArticlesIcon />,
+    path: '#',
+  },
+  {
+    label: 'Settings',
+    icon: <SettingsIcon />,
+    path: '#',
+    isExtra: true,
+  },
+  {
+    label: 'Subscription',
+    icon: <SubscriptionIcon />,
+    path: '#',
+    isExtra: true,
+  },
+];
+
+export default routes;

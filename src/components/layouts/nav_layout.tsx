1565d40def4329ad41a33047988c5b1c1eddff89
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 11:08:39 -0600

    Styles in NavLayout changed

--- src/components/layouts/nav_layout.tsx
+++ src/components/layouts/nav_layout.tsx
@@ -1,23 +1,25 @@
 import { LogoNav } from '@/svg/logo';
 import { NavLayoutProps } from '@/types/components/dashboard';
+import menuDesktop from '../nav/nav_desktop';
+import routes from '@/utils/data/routes';
 
 const NavLayout = ({ children }: NavLayoutProps) => {
   return (
     <div className="flex">
-      <div className="flex flex-none h-screen sm:w-[76px] lg:w-[255px] bg-primary-bg-black justify-center">
-        <div className="flex sm:mb-2 sm:mt-6">
-          <div className="flex-auto hidden sm:block">
-            <a href="#">
+      <div className="flex flex-col flex-none h-screen sm:w-[76px] lg:w-[255px] bg-primary-bg-black">
+        <div className="flex sm:mb-6 sm:mt-6 items-center justify-center">
+          <div className="flex-none w-20 hidden sm:block">
+            <a href="#" className="flex items-center justify-center">
               <LogoNav />
             </a>
           </div>
-          <div className="hidden lg:block flex-auto">
-            <h1 className="text-[19px] font-[700] text-secondary-gray/70 tracking-[0.4px] leading-normal ml-3">
+          <div className="hidden lg:block flex-1">
+            <h1 className="text-[19px] font-[700] text-secondary-gray/70 tracking-[0.4px] leading-normal">
               Dashboard Kit
             </h1>
           </div>
-          <div></div>
         </div>
+        <ul className="mt-6">{menuDesktop(routes)}</ul>
       </div>
       <div className="flex-auto">{children}</div>
     </div>

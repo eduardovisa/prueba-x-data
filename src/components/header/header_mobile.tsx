279fbbf1f12e98715003a26639c7bb72ab6a548b
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-18 19:14:42 -0600

    Replace a to link

--- src/components/header/header_mobile.tsx
+++ src/components/header/header_mobile.tsx
@@ -1,13 +1,14 @@
 import { BellIcon } from '@/svg/header';
 import { LogoMobileNav } from '@/svg/logo';
+import Link from 'next/link';
 
 const MobileHeader = () => {
   return (
     <div className="bg-primary-bg-black h-[60px] sm:hidden flex justify-between items-center p-4">
       <div className="flex-1 cursor-pointer">
-        <a href="/">
+        <Link href="/">
           <LogoMobileNav />
-        </a>
+        </Link>
       </div>
       <div className="flex-none cursor-pointer">
         <BellIcon />
--- src/components/layouts/login_layout.tsx
+++ src/components/layouts/login_layout.tsx
@@ -1,5 +1,6 @@
 import { LogoLogin } from '@/svg/logo';
 import { LoginLayoutProps } from '@/types/components/login';
+import Link from 'next/link';
 
 const LoginLayout = ({ children }: LoginLayoutProps) => {
   return (
@@ -7,9 +8,9 @@ const LoginLayout = ({ children }: LoginLayoutProps) => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
         <div className="bg-white rounded-lg shadow w-380 h-710 sm:w-600 sm:h-582 lg:w-380">
           <div className="h-full bg-[#fff] rounded-[8px] p-6 flex flex-col items-center justify-center">
-            <a href="/" className="flex items-center mb-2">
+            <Link href="/" className="flex items-center mb-2">
               <LogoLogin />
-            </a>
+            </Link>
             <h1 className="text-[19px] font-[700] text-secondary-gray/70 tracking-[0.4px] mb-6 leading-normal">
               Dashboard Kit
             </h1>
